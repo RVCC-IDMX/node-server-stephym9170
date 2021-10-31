dfd7a03834d219963e9ceecd3a28af991e2ef99d
Your Name <stephym917@gmail.com>
2021-10-31 14:50:28 -0400

    Basic HTML Content

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
-
+ 
+ 
 /* Global Properties */
 console.log(__filename);
 console.log(__dirname);
-
+ 
 /*Reading Files*/
 const {readFile, readFileSync} = require("fs");
 try{
@@ -26,14 +27,14 @@ console.log(data);
 } catch (err) {
 console.error(err);
 }
-
+ 
 console.log("Log from outside");
-
+ 
 /* Writing Files */
 const{appendFile} = require("fs");
-
+ 
 const newContent = "\n This is some more new text";
-
+ 
 appendFile("hi.txt", newContent, (err) => {
     if (err){
         console.error(err);
@@ -41,10 +42,10 @@ appendFile("hi.txt", newContent, (err) => {
     }
     console.log("content written");
 })
-
+ 
 /* Renaming and Deleting Files */
 const{ unlink } = require("fs");
-
+ 
 unlink("hello.txt", (err) => {
     if (err) {
         console.error(err);
@@ -52,9 +53,25 @@ unlink("hello.txt", (err) => {
     }
     console.log("File deleted!");
 })
-
+ 
 /* Export and Import */
 import {addNums} from "./addNums.js";
-
+ 
 const sum = addNums(2, 2);
 console.log(sum);
+
+/* Basic HTML Content */
+const http = require('http');
+
+const PORT = process.env.PORT || 3000
+
+const server = http
+.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader("Content Type", "text/html")
+    res.write("Hello World");
+    res.write("Hello World again");
+    res.end("The End");
+})
+server.listen(PORT, () => console.log(`Server listening on port 
+${PORT}`));
