898df7975a931648fd83ada1fc7fa085496e3ded
Your Name <stephym917@gmail.com>
2021-10-31 14:51:24 -0400

    Basic Web server html files

--- server.js
+++ server.js
@@ -61,17 +61,23 @@ const sum = addNums(2, 2);
 console.log(sum);
 
 /* Basic HTML Content */
-const http = require('http');
+const http = require("http");
+const fs = require ("fs")
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http
 .createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content Type", "text/html")
-    res.write("Hello World");
-    res.write("Hello World again");
-    res.end("The End");
-})
+    res.statusCode = 200;
+    res.setHeader("Content Type", "text/html");
+    fs.readFile("./index.html", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.end();
+        } else {
+        res.end(data);
+        }
+    })
+    });
 server.listen(PORT, () => console.log(`Server listening on port 
 ${PORT}`));
